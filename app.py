22e2ab45ceb3ec66bc6bfec50a99fa687d009c49
Eleandro1981 <projetos@proje.toys>
2025-04-03 11:05:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from simulador_inj import exibir_simulador_inj
-from crm_cadastro_cliente import exibir_crm_cadastro_cliente
+from simulador import exibir_simulador
+from crm import exibir_crm
 
 st.set_page_config(page_title="App de Moldes", layout="wide")
 st.title("🛠️ Plataforma de Projetos de Moldes")
@@ -15,10 +15,10 @@ menu = st.sidebar.radio("Navegar entre os módulos:", [
 ])
 
 if menu == "Simulador de Injeção":
-    exibir_simulador_inj()
+    exibir_simulador()
 
 elif menu == "CRM - Cadastro de Clientes":
-    exibir_crm_cadastro_cliente()
+    exibir_crm()
 
 elif menu == "Orçamento de Moldes":
     st.subheader("📦 Orçamento de Moldes")
