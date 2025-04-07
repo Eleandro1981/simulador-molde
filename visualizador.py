3f4c3d54b13c4d0c9a3005f8898c2e6396a4b402
Eleandro1981 <projetos@proje.toys>
2025-04-07 17:31:41 -0300

    Update visualizador.py

--- visualizador.py
+++ visualizador.py
@@ -1,21 +1,19 @@
 import streamlit as st
-from st3d import st_3d_viewer
+from streamlit_vtkjs import st_vtkjs
 import os
 
 def exibir_visualizador():
     st.subheader("🧊 Visualizador 3D - Arquivos STL")
 
-    # Lista os arquivos STL disponíveis
     arquivos = [f for f in os.listdir("arquivos_3d") if f.endswith(".stl")]
 
     if not arquivos:
-        st.warning("Nenhum arquivo STL encontrado na pasta 'arquivos_3d'.")
+        st.warning("Nenhum arquivo STL encontrado.")
         return
 
-    # Dropdown para o usuário escolher o arquivo
-    selecionado = st.selectbox("Selecione um arquivo STL para visualizar:", arquivos)
+    escolhido = st.selectbox("Selecione o arquivo STL:", arquivos)
 
-    caminho = os.path.join("arquivos_3d", selecionado)
-
-    # Visualização 3D
-    st_3d_viewer(caminho)
+    caminho = os.path.join("arquivos_3d", escolhido)
+    
+    with open(caminho, "rb") as f:
+        st_vtkjs(file=f.read())
